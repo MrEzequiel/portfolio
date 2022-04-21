8c2bb303cbcb67cb7ffa66ff85a3edc2fc45b949
Ezequiel Soares <mr.ezequiel.soares@gmail.com>
2022-04-20 23:26:51 -0500

    feat: :sparkles: knowledge table

--- src/components/HomeSections/Knowledge/index.tsx
+++ src/components/HomeSections/Knowledge/index.tsx
@@ -1,28 +1,69 @@
 import Image from 'next/image'
-import { FC } from 'react'
+import { FC, useState } from 'react'
+import { useTheme } from 'styled-components'
 import IKnowledge from '../../../interfaces/IKnowledge'
 import Section from '../../../styles/layout/Section'
+import Text from '../../../styles/layout/Text'
 import TitleSection from '../../TitleSection'
-import { KnowledgeButton, KnowledgeContainer } from './styles'
+import {
+  KnowledgeBody,
+  KnowledgeContent,
+  KnowledgeItem,
+  KnowledgeList
+} from './styles'
 
 interface KnowledgeProps {
   knowledge: IKnowledge[]
 }
 
 const Knowledge: FC<KnowledgeProps> = ({ knowledge }) => {
+  const theme = useTheme()
+  const [selectedKnowledge, setSelectedKnowledge] = useState<IKnowledge>(
+    knowledge[0]
+  )
+
   return (
     <Section id="knowledge" patternPositionY="bottom" patternPositionX="right">
       <TitleSection alignCenter>Conhecimentos</TitleSection>
 
-      <KnowledgeContainer>
-        {knowledge.map(({ description, icon, id, name }) => (
-          <KnowledgeButton key={id}>
-            <div className="icon-container">
-              <Image src={icon} alt={name} layout="fill" objectFit="contain" />
-            </div>
-          </KnowledgeButton>
-        ))}
-      </KnowledgeContainer>
+      <KnowledgeContent>
+        <KnowledgeList>
+          {knowledge.map(knowledgeItem => (
+            <KnowledgeItem
+              key={knowledgeItem.id}
+              isSelected={selectedKnowledge.id === knowledgeItem.id}
+              onClick={() => {
+                if (selectedKnowledge.id === knowledgeItem.id) return
+
+                setSelectedKnowledge(knowledgeItem)
+              }}
+            >
+              <div className="icon-container">
+                <Image
+                  src={knowledgeItem.icon}
+                  alt={knowledgeItem.name}
+                  layout="fill"
+                  objectFit="contain"
+                />
+              </div>
+            </KnowledgeItem>
+          ))}
+        </KnowledgeList>
+
+        <KnowledgeBody>
+          <Text variant="heading2" as="h3">
+            {selectedKnowledge.name}
+          </Text>
+
+          <Text
+            variant="body3"
+            as="p"
+            style={{ marginTop: theme.spaces.extra_small_2 }}
+          >
+            {selectedKnowledge.description}
+          </Text>
+        </KnowledgeBody>
+      </KnowledgeContent>
     </Section>
   )
 }
--- src/components/HomeSections/Knowledge/styles.ts
+++ src/components/HomeSections/Knowledge/styles.ts
@@ -1,20 +1,50 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const KnowledgeContainer = styled.ul`
-  display: flex;
+export const KnowledgeContent = styled.div`
   margin-top: ${props => props.theme.spaces.small_3};
 
   width: min-content;
   margin-left: auto;
   margin-right: auto;
+
+  @media (max-width: 700px) {
+    display: flex;
+    width: fit-content;
+  }
 `
 
-export const KnowledgeButton = styled.li`
+export const KnowledgeList = styled.ul`
+  display: flex;
+
+  @media (max-width: 700px) {
+    flex-direction: column;
+  }
+`
+
+interface KnowledgeItemProps {
+  isSelected: boolean
+}
+
+export const KnowledgeItem = styled.li<KnowledgeItemProps>`
+  cursor: pointer;
+
   display: flex;
   justify-content: center;
   align-items: center;
   width: 75px;
   height: 75px;
+
+  background: ${props => props.theme.colorsGrey.g10};
+  transition: 750ms ease;
+  transition-property: background box-shadow;
+
+  &:hover {
+    background: ${props => props.theme.colorsGrey.g9};
+
+    .icon-container {
+      opacity: 1;
+    }
+  }
 
   &:first-child {
     border-radius: 4px 0px 0px 0px;
@@ -28,5 +58,51 @@ export const KnowledgeButton = styled.li`
     position: relative;
     width: 30px;
     height: 30px;
+    opacity: 0.5;
+    transition: opacity 750ms ease;
   }
+
+  ${props =>
+    props.isSelected &&
+    css`
+      transition-property: background border-radius box-shadow border transform;
+
+      cursor: default;
+
+      transform: translateY(4px);
+      position: relative;
+      z-index: 1;
+
+      background: ${props => props.theme.colorsGrey.g10} !important;
+      box-shadow: 0 -8px 8px 0px rgba(0, 0, 0, 0.1);
+      border-radius: 30px 4px 0px 0px !important;
+      border: 5px solid ${props => props.theme.colorsPrimary.p800};
+      border-bottom: none;
+      box-sizing: border-box;
+
+      .icon-container {
+        opacity: 1;
+      }
+
+      @media (max-width: 700px) {
+        border-radius: 5px 0 0 30px !important;
+        box-shadow: -8px 0px 8px 0px rgba(0, 0, 0, 0.1);
+        transform: translateX(5px);
+        border-bottom: 5px solid ${props => props.theme.colorsPrimary.p800};
+        border-right: none;
+      }
+    `}
+`
+
+export const KnowledgeBody = styled.div`
+  position: relative;
+  top: -1px;
+
+  background: ${props => props.theme.colorsGrey.g10};
+  /* color-primary */
+
+  border: 5px solid ${props => props.theme.colorsPrimary.p800};
+  border-radius: 4px;
+  padding: ${props => props.theme.spaces.small_2};
+  width: 100%;
 `
