96007c436b96e885b47917cb2db02a6ed5eaa0e0
Ezequiel Soares <mr.ezequiel.soares@gmail.com>
2024-04-09 15:25:17 -0500

    refactor: experience item component

---
+++ src/components/HomeSections/Experience/ExperienceItem.tsx
@@ -0,0 +1,131 @@
+import { useTheme } from "styled-components"
+import useMediaQuery from "../../../hooks/useMediaQuery"
+
+import Image from "next/image"
+import IExperience from "../../../interfaces/IExperience"
+
+import { Chip } from "../../../styles/layout/Chip"
+import Text from "../../../styles/layout/Text"
+import {
+  ExperienceDescripitonText,
+  FooterExperience,
+  TechsList,
+  ExperienceItem as ExperienceItemContainer
+} from "./styles"
+
+import { format } from "date-fns"
+import { ptBR } from "date-fns/locale"
+
+
+interface ExperienceItemProps {
+  experience: IExperience
+}
+
+export const ExperienceItem = ({ experience }: ExperienceItemProps) => {
+  const theme = useTheme()
+  const isMobile = useMediaQuery('(max-width: 450px)')
+
+  const getDifferenceInMilliseconds = (dateStart: string, dateEnd?: string) => {
+    const startDate = new Date(dateStart)
+    const endDate = dateEnd ? new Date(dateEnd) : new Date()
+    return endDate.getTime() - startDate.getTime()
+  }
+
+  const formateDate = (date: string) => {
+    return format(new Date(date), 'LLLL yyyy', { locale: ptBR })
+  }
+
+  const getPlurals = (number: number, singular: string, plural: string) => {
+    return number > 1 ? plural : singular
+  }
+
+  const getExperienceDuration = (dateStart: string, dateEnd?: string) => {
+    const totalTime = getDifferenceInMilliseconds(dateStart, dateEnd)
+
+    const YEAR_IN_MILISECONDS = 1000 * 60 * 60 * 24 * 365
+    const MONTH_IN_MILISECONDS = 1000 * 60 * 60 * 24 * 30
+
+    const years = Math.floor(totalTime / YEAR_IN_MILISECONDS)
+    const totalTimeWithoutYears = totalTime - (years * YEAR_IN_MILISECONDS)
+
+    const months = Math.ceil(totalTimeWithoutYears / MONTH_IN_MILISECONDS)
+
+    const monthsLabel = `${months} ${getPlurals(months, 'mês', 'meses')}`
+    const yearsLabel = `${years} ${getPlurals(years, 'ano', 'anos')}`
+
+    if (years > 0 && months > 0) {
+      return `${yearsLabel} e ${monthsLabel}`
+    }
+
+    if (years > 0) {
+      return yearsLabel;
+    }
+
+    return monthsLabel
+  }
+
+  const getDateExperience = (dateStart: string, dateEnd?: string) => {
+    const startDate = formateDate(dateStart)
+    const endDate = dateEnd ? formateDate(dateEnd) : 'Presente'
+
+    return `${startDate} - ${endDate} (${getExperienceDuration(
+      dateStart,
+      dateEnd
+    )})`
+  }
+
+  return (
+    <ExperienceItemContainer key={experience.id}>
+      <div className="experience-header">
+        <div className="company-logo">
+          <Image
+            fill
+            src={experience.logo_company}
+            alt={experience.title}
+          />
+        </div>
+        <div className="text">
+          <Text
+            as="h3"
+            variant={isMobile ? 'heading1' : 'heading2'}
+            style={{ color: theme.colorsPrimary.p1400 }}
+          >
+            {experience.title}
+          </Text>
+          <Text as="p" variant="body3">
+            {experience.company} ·{' '}
+            <Text
+              as="span"
+              variant="body3"
+              style={{ color: theme.colorsGrey.g6 }}
+            >
+              {getDateExperience(
+                experience.date_start,
+                experience.date_end
+              )}
+            </Text>
+          </Text>
+        </div>
+      </div>
+
+      <ExperienceDescripitonText as="p">
+        {experience.description}
+      </ExperienceDescripitonText>
+
+      <FooterExperience>
+        <TechsList>
+          <Text
+            as="span"
+            variant="body3"
+            style={{ fontWeight: 700 }}
+          >
+            Tecnologias:
+          </Text>
+          {experience.techs.map(tech => (
+            <Chip key={tech}>{tech}</Chip>
+          ))}
+        </TechsList>
+      </FooterExperience>
+    </ExperienceItemContainer>
+  )
+}
--- src/components/HomeSections/Experience/index.tsx
+++ src/components/HomeSections/Experience/index.tsx
@@ -1,79 +1,20 @@
 import { FC } from 'react'
-import Image from 'next/image'
-
-import { useTheme } from 'styled-components'
-import useMediaQuery from '../../../hooks/useMediaQuery'
 
 import TitleSection from '../../TitleSection'
-import Text from '../../../styles/layout/Text'
 import Section from '../../../styles/layout/Section'
-import { Chip } from '../../../styles/layout/Chip'
 import {
   ExperienceContainer,
-  ExperienceDescripitonText,
-  ExperienceItem,
   ExperienceList,
-  FooterExperience,
-  TechsList
 } from './styles'
 
 import IExperience from '../../../interfaces/IExperience'
-import SkeletonPlaceholder from '../../../utils/SkeletonPlaceholder'
-import {
-  differenceInDays,
-  format,
-  differenceInCalendarYears,
-  differenceInCalendarMonths
-} from 'date-fns'
-import { ptBR } from 'date-fns/locale'
+import { ExperienceItem } from './ExperienceItem'
 
 interface ExperienceProps {
   experiences: IExperience[]
 }
 
 const Experience: FC<ExperienceProps> = ({ experiences }) => {
-  const theme = useTheme()
-  const isMobile = useMediaQuery('(max-width: 450px)')
-
-  const formateDate = (date: string) => {
-    return format(new Date(date), 'LLL yyyy', { locale: ptBR })
-  }
-
-  const getExperienceDuration = (start: string, end?: string) => {
-    const startDate = new Date(start)
-    const endDate = end ? new Date(end) : new Date()
-
-    if (endDate.getFullYear() - startDate.getFullYear() > 0) {
-      const years = differenceInCalendarYears(endDate, startDate)
-      startDate.setFullYear(endDate.getFullYear())
-      const months = differenceInCalendarMonths(endDate, startDate)
-      if (months > 0) {
-        return `${years} ${years > 1 ? 'anos' : 'ano'} e ${months} ${
-          months > 1 ? 'meses' : 'mês'
-        }`
-      }
-      return `${years} ano${years > 1 ? 's' : ''}`
-    }
-
-    if (endDate.getMonth() - startDate.getMonth() > 0) {
-      const months = differenceInCalendarMonths(endDate, startDate)
-      return `${months} ${months > 1 ? 'meses' : 'mês'}`
-    }
-
-    const days = differenceInDays(endDate, startDate)
-    return `${days} dia${days > 1 ? 's' : ''}`
-  }
-
-  const getDateExperience = (dateStart: string, dateEnd?: string) => {
-    const startDate = formateDate(dateStart)
-    const endDate = dateEnd ? formateDate(dateEnd) : 'Presente'
-
-    return `${startDate} - ${endDate} (${getExperienceDuration(
-      dateStart,
-      dateEnd
-    )})`
-  }
-
   return (
     <Section
       disablePattern
@@ -89,58 +30,7 @@ const Experience: FC<ExperienceProps> = ({ experiences }) => {
       <ExperienceContainer>
         <ExperienceList>
           {experiences.map(experience => (
-            <ExperienceItem key={experience.id}>
-              <div className="experience-header">
-                <div className="company-logo">
-                  <Image
-                    fill
-                    src={experience.logo_company}
-                    alt={experience.title}
-                  />
-                </div>
-                <div className="text">
-                  <Text
-                    as="h3"
-                    variant={isMobile ? 'heading1' : 'heading2'}
-                    style={{ color: theme.colorsPrimary.p1400 }}
-                  >
-                    {experience.title}
-                  </Text>
-                  <Text as="p" variant="body3">
-                    {experience.company} ·{' '}
-                    <Text
-                      as="span"
-                      variant="body3"
-                      style={{ color: theme.colorsGrey.g6 }}
-                    >
-                      {getDateExperience(
-                        experience.date_start,
-                        experience.date_end
-                      )}
-                    </Text>
-                  </Text>
-                </div>
-              </div>
-
-              <ExperienceDescripitonText as="p">
-                {experience.description}
-              </ExperienceDescripitonText>
-
-              <FooterExperience>
-                <TechsList>
-                  <Text
-                    as="span"
-                    variant="body3"
-                    style={{ fontWeight: 700 }}
-                  >
-                    Tecnologias:
-                  </Text>
-                  {experience.techs.map(tech => (
-                    <Chip key={tech}>{tech}</Chip>
-                  ))}
-                </TechsList>
-              </FooterExperience>
-            </ExperienceItem>
+            <ExperienceItem key={experience.id} experience={experience} />
           ))}
         </ExperienceList>
       </ExperienceContainer>
